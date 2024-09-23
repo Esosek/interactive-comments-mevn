734e1c6098e4f80ecb2c10abb0780173a0a68180
Ales Zima <ales@cpex.cz>
2024-09-23 17:23:25 +0200

    Added tailwind color config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      'blue-200': 'hsl(239, 57%, 85%)',
+      'blue-500': 'hsl(238, 40%, 52%)',
+      'blue-800': 'hsl(212, 24%, 26%)',
+      'red-200': 'hsl(357, 100%, 86%)',
+      'red-500': 'hsl(358, 79%, 66%)',
+      'neutral-100': 'hsl(228, 33%, 97%)',
+      'neutral-200': 'hsl(223, 19%, 93%)',
+      'neutral-400': 'hsl(211, 10%, 45%)'
+    },
     extend: {}
   },
   plugins: []
